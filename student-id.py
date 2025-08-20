340836981af6fcd092220da0cba88eea5cc67b5d
José <rmzspencerjose@outlook.com>
2025-08-20 20:20:40 +0000

    class 6

---
+++ student-id.py
@@ -0,0 +1,13 @@
+def main():
+	print('')
+	print('Please enter the following information to create your ID Card.')
+	input('Press enter to continue.')
+
+main ()
+
+fname = input("first name: ").title()
+lname = input("last name: ").capitalize()
+email = input("email address: ")
+phone = input("phone number: ")
+id = input("Student or Teacher ID number: ")
+fpt = input("class you're in: ")
