05245b2d0b0a01a8b22b4392147b9f6d6edb3c39
José <rmzspencerjose@outlook.com>
2025-08-25 19:39:28 +0000

    class 8

---
+++ goblin.py
@@ -0,0 +1,16 @@
+import random
+print("welcome to the goblin game")
+print("THE BEST GAME EVER")
+player_name = input("Write your name: ")
+print(f"{player_name}, can you find the goblin")
+print("|_|" * 50)
+goblin_position = random.randint(1, 50)
+keep_trying = True
+while keep_trying:
+    guess_position = int(input("can you guess where the goblin is? "))
+
+    if goblin_position == guess_position:
+        print("you won")
+        keep_trying = False
+    else: 
+        print("NO")
