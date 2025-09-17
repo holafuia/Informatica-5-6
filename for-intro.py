087c465454093ad791218d94f20cfdc135c80a9c
José <rmzspencerjose@outlook.com>
2025-09-17 20:21:14 +0000

    class 19

---
+++ for-intro.py
@@ -0,0 +1,16 @@
+doctrine = ["faith", "repentance", "baptism", "holy ghost", "endure to the end"]
+
+# i = 0
+# while 1 < len(doctrine):
+#     print(doctrine(i))
+#     i += 1
+
+for principle in doctrine:
+    print (principle)
+
+school_objective = ["deepen conversion to Jesus Christ and His restored Gospel", "Qualify for the blessing of the Temple", 
+"Prepare for missionary and Church service", "Build the kingdom of God"]
+
+for objective in school_objective:
+    print(objective)
+    print("👌")
