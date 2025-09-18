45c57c072e080d16012b8c22e949e245e6870c02
José <rmzspencerjose@outlook.com>
2025-09-18 20:16:49 +0000

    class 20.2

--- file-io.py
+++ file-io.py
@@ -3,10 +3,18 @@
 # for i in range(3): #la mugre repite 3 veces
 #     names.append(input("What's your name? "))
 
-# for name in sorted(names):
+# for name in sorted(names): # sortea los de arriba
 #     print(f"Hello {name}")
 
-name = (input("What's your name? "))
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+# name = (input("What's your name? ")) # agrega texto
+# file = open("names.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+# with open("names.txt", "a") as file: # lo mismo que el de arriba
+#     file.write(f"{input("What's your name? ")}")
+
+with open("names.txt", "r") as file: # it let's you see the text file
+    lines = file.readlines()
+for line in sorted(lines):
+    print(line.strip())
