56c9ddb3e1c9248f223a12cf13959431289e51e4
José <rmzspencerjose@outlook.com>
2025-10-01 20:18:25 +0000

    class 29

--- character-count.py
+++ character-count.py
@@ -1,9 +1,28 @@
+def main():
+    message = input("Write your message: ")
+    count = {}
+    dictionary(message, count)
+
 def dictionary(message, count):
     for x in message:
-        count[x] = +1
-    for y in count:
-        print(f"{y}: {count[y]}")
+        count.setdefault(x, 0)
+        count[x] += 1
+    for x in count:
+        print(f"{x}: {count[x]}")
+    
+    print(f"The length is: {len(message)}")
 
-message = input("Write your message: ")
-count = {}
-dictionary(message, count)
+    values = count[x]
+    for y in count:
+        value = count[y]
+        if int(value) >= values:
+            values = value
+    maxs = []
+    for a in count:
+        value = count[a]
+        if int(value) == values:
+            maxs += a
+
+    print(f"The letter(s) with most appearence are {maxs} with {values} letters each")
+
+main()
