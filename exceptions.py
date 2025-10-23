646df0050284e58bdb0fd464fa77788b9e9318da
José <rmzspencerjose@outlook.com>
2025-10-23 19:58:09 +0000

    class 33.1

--- exceptions.py
+++ exceptions.py
@@ -20,11 +20,24 @@
 # print(spam(0))
 # print(spam(1))
 
-while True:
-    try:
-        x = int(input("what's x? "))
-    except ValueError:
-        print("x is not a number")
-    else:
-        print(f"x is equal t0 {x}")
-        break
+# while True:
+#     try:
+#         x = int(input("what's x? "))
+#     except ValueError:
+#         print("x is not a number")
+#     else:
+#         print(f"x is equal t0 {x}")
+#         break
+
+def read_small_integer():
+    while True:
+        try:
+            input_str = input("Please type in an integer: ")
+            number = int(input_str)
+            if number < 100 and number >= 0:
+                return number
+        except ValueError:
+            pass
+        
+number = read_small_integer()
+print(number, "to the power of 3 is: ", number**3)
