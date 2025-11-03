c7ff1d9b105df29933ac722aa7aa015c33afbfd1
José <rmzspencerjose@outlook.com>
2025-11-03 19:40:33 +0000

    class 39

---
+++ libraries.py
@@ -0,0 +1,24 @@
+# import random
+
+# coin = random.choice(["heads", "tails"])
+# print (coin)
+
+# number = random.randint(1, 10)
+# print(number)
+
+# cards = ["jack", "queen", "king", "ace"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
+# import statistics
+import sys
+import cowsay
+# print(statistics.mean([int(sys.argv[1]), int(sys.argv[2])]))
+
+# print("Hello, my name is", sys.argv[1])
+
+try:
+    cowsay.cow(sys.argv[1])
+except IndexError:
+    sys.exit()
